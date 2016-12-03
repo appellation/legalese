41e9d6565d3eea8c4f5f074ac91aa689e9368917
Will Nelson <nelson.will@live.com>
2016-12-03 13:15:38 -0800

    Bills

---
+++ routes/bills.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nelso on 12/3/2016.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res)  {
+    //
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/bills', require('./bills'));
+
 module.exports = router;
